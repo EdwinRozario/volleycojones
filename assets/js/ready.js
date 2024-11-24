3e70ef071a554aa8768e2b96461140bf53eeab98
edwinrozario <rozarioed@gmail.com>
2024-11-24 17:17:35 +1300

    feat(): has score board

--- assets/js/ready.js
+++ assets/js/ready.js
@@ -1,19 +1,25 @@
 $(document).ready(function(){
-	const today = new Date().toISOString().split('T')[0].replaceAll('-', '');
-	const table = document.getElementById('next');
-	let nextDay = null;
-	const schedule = new Map([
-		[20241010, { time: '10 Oct, 7:40 pm', team: 'Twenty Twenty', court: 3, duty: 'NA' }],
-		[20241017, { time: '17 Oct, 6:50 pm', team: 'Team Lang', court: 2, duty: 'NA' }],
-		[20241024, { time: '24 Oct, 6:00 pm', team: 'Kilbirnie Kreamers', court: 3, duty: 'NA' }],
-		[20241031, { time: '31 Oct, 6:50 pm', team: 'Amy & Co', court: 2, duty: '6:00 pm' }],
-		[20241107, { time: '07 Nov, 6:00 pm', team: 'Spike the Stegosaurus', court: 3, duty: 'NA' }],
-		[20241114, { time: '14 Nov, 7:40 pm', team: 'Twenty Twenty', court: 3, duty: 'NA' }],
-		[20241121, { time: '21 Nov, 6:50 pm', team: 'Team Lang', court: 2, duty: '6:00 pm' }],
-		[20241128, { time: '28 Nov, 6:00 pm', team: 'Kilbirnie Kreamers', court: 3, duty: 'NA' }],
-		[20241205, { time: '05 Dec, 6:50 pm', team: 'Amy & Co', court: 2, duty: 'NA' }],
-		[20241212, { time: '12 Dec, 6:00 pm', team: 'Spike the Stegosaurus', court: 3, duty: 'NA' }]
-	]);
+	populateSchedule();
+
+	populateNextGame();
+});
+
+const schedule = new Map([
+	[20241010, { time: '10 Oct, 7:40 pm', team: 'Twenty Twenty', court: 3, duty: 'NA' }],
+	[20241017, { time: '17 Oct, 6:50 pm', team: 'Team Lang', court: 2, duty: 'NA' }],
+	[20241024, { time: '24 Oct, 6:00 pm', team: 'Kilbirnie Kreamers', court: 3, duty: 'NA' }],
+	[20241031, { time: '31 Oct, 6:50 pm', team: 'Amy & Co', court: 2, duty: '6:00 pm' }],
+	[20241107, { time: '07 Nov, 6:00 pm', team: 'Spike the Stegosaurus', court: 3, duty: 'NA' }],
+	[20241114, { time: '14 Nov, 7:40 pm', team: 'Twenty Twenty', court: 3, duty: 'NA' }],
+	[20241121, { time: '21 Nov, 6:50 pm', team: 'Team Lang', court: 2, duty: '6:00 pm' }],
+	[20241128, { time: '28 Nov, 6:00 pm', team: 'Kilbirnie Kreamers', court: 3, duty: 'NA' }],
+	[20241205, { time: '05 Dec, 6:50 pm', team: 'Amy & Co', court: 2, duty: 'NA' }],
+	[20241212, { time: '12 Dec, 6:00 pm', team: 'Spike the Stegosaurus', court: 3, duty: 'NA' }]
+]);
+
+
+function populateSchedule(){
+	const table = document.getElementById('schedule');
 
 	schedule.keys().forEach(key => {
 		var game = schedule.get(key);
@@ -25,6 +31,11 @@ $(document).ready(function(){
 		time.innerHTML = game.time;
 		team.innerHTML = game.team;
 	});
+}
+
+function populateNextGame() {
+	const today = new Date().toISOString().split('T')[0].replaceAll('-', '');
+	let nextDay = null;
 
 	schedule.keys().forEach(key => {
 		if (today <= key) {
@@ -32,7 +43,7 @@ $(document).ready(function(){
 				nextDay = key;
 
 				var element = document.getElementById(key);
-				element.classList.add('next-game');
+				element.classList.add('highlight');
 			}
 		}
 	});
@@ -45,4 +56,4 @@ $(document).ready(function(){
 		document.getElementById('court').innerHTML = nextGame.court;
 		document.getElementById('duty').innerHTML = nextGame.duty;
 	}
-});
+}
