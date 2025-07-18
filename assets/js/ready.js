1a7d1b1c73e46fab9466b768a57a0fe3d3ee9887
Edwin Rozario <edwin.rozario@storypark.com>
2025-07-18 15:36:33 +1200

    chore(scores & scehdule): 18 July 2025

--- assets/js/ready.js
+++ assets/js/ready.js
@@ -5,16 +5,20 @@ $(document).ready(function(){
 });
 
 const schedule = new Map([
-	[20250710, { time: '10 July, 6:00 pm', team: 'Spike the Stegasaurus', court: 2, duty: 'NA' }]
+	[20250717, { time: '17 July, 8:30 pm', team: 'Amy & Co', court: 1, duty: 'NA' }],
+	[20250724, { time: '24 July, 6:00 pm', team: 'Kilbirnie Kreamers', court: 2, duty: 'NA' }],
+	[20250731, { time: '31 July, 6:50 pm', team: 'Spike the Stegasaurus', court: 2, duty: '6:00 pm' }],
+	[20250807, { time: '07 Aug, 7:40 pm', team: 'Dirty Dogs', court: 3, duty: 'NA' }],
+	[20250814, { time: '14 Aug, 8:30 pm', team: 'Twenty Twenty', court: 3, duty: 'NA' }]
 ]);
 
 const scores = [
-	{ team: 'Volley Cojones',        score: 9.5, wins: 1, draws: 1, losses: 7 },
-	{ team: 'Team Lang',             score: 30.5,  wins: 6, draws: 1, losses: 2 },
-	{ team: 'Amy & Co',              score: 28.5,  wins: 7, draws: 0, losses: 2 },
-	{ team: 'Kiss my Ace',           score: 7, wins: 1, draws: 0, losses: 8 },
-	{ team: 'Twenty Twenty',         score: 20, wins: 5, draws: 0, losses: 4 },
-	{ team: 'Spike the Stegosaurus', score: 28,  wins: 6, draws: 0, losses: 3 }
+	{ team: 'Volley Cojones',        score: 13.5, wins: 2, draws: 1, losses: 7 },
+	{ team: 'Team Lang',             score: 34.5,  wins: 7, draws: 1, losses: 2 },
+	{ team: 'Amy & Co',              score: 33,  wins: 8, draws: 0, losses: 2 },
+	{ team: 'Kiss my Ace',           score: 7, wins: 1, draws: 0, losses: 9 },
+	{ team: 'Twenty Twenty',         score: 20.5, wins: 5, draws: 0, losses: 5 },
+	{ team: 'Spike the Stegosaurus', score: 28.5,  wins: 6, draws: 0, losses: 4 }
 ];
 
 function populateScores() {
